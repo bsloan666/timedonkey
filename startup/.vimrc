b29b1c57f50d092ee84941b0ec3c3663c7421732
Blake Sloan <bsloan@marvelstudios.com>
2021-08-04 15:45:41 -0700

    updated illustrations

--- startup/.vimrc
+++ startup/.vimrc
@@ -1,19 +1,14 @@
-autocmd!
-set expandtab
-set tabstop=4
-set shiftwidth=4
-set autoindent
-set smartindent
-set nowrap
-set pastetoggle=<F2>
-syntax on
+" Put your .vimrc customizations above this line. 
 
+
+" TimeDonkey vim plugin
 function SaveRecord(app, action)
     let com = "echo `date +\"%Y-%m-%d %H:%M:%S\"`," . a:app . "," . a:action . "," . @% . " >> ~/.activitylog"
     call system(com)
     return 0
 endfunction
 
+" Events that call TimeDonkey::SaveRecord
 autocmd BufWinEnter * call SaveRecord('vi', 'open')
 autocmd BufWritePost * call SaveRecord('vi', 'save')
 autocmd BufWinLeave * call SaveRecord('vi', 'close')
--- startup/menu.py
+++ startup/menu.py
@@ -1,16 +1,23 @@
+# this is a python startup script that will be run automatically 
+# when Nuke (https://learn.foundry.com/nuke/13.0/content/learn_nuke.html) 
+# is launched
+
+
 import os
 import datetime
 import nuke
 
 
 def save_record(app, action):
+    """
+    Save a time-stamped event in the users TimeDonkey activity log.  
+    """
     current = datetime.datetime.now()
     timestr = current.strftime("%Y-%m-%d %H:%M:%S")
     if nuke.root().name() == 'Root' or action == 'save':
         with open('{0}/.activitylog'.format(os.getenv('HOME')), 'a') as handle:
             handle.write(timestr + ',' + app + ',' + action + ',' + os.path.basename(nuke.root().name()) + '\n')
         
-
 
 save_record('nuke', 'open')
 nuke.addOnScriptSave(save_record, args=('nuke', 'save'))
